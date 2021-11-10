cf8ab94eff4818bc5bcac3cc0a4b434a8c66cab6
Monkey322 <otta12@gmail.com>
2021-11-09 21:32:27 -0300

    Arquivos criado para respostas

---
+++ random/string.js
@@ -0,0 +1,57 @@
+//aprendendo a contatenar
+
+/*console.log("hallo" + ' ' + "Welt")
+
+
+
+console.log("hallo" + "welt")
+
+myName = "Otavio"
+
+console.log("hallo" + " " + myName)
+
+myname2 = 2
+
+console.log("Hallo" + myName2)*/
+
+// ------------------------------
+
+/*var x=0
+y = "morango"
+console.log(x, "\n" ,y)*/
+
+/*let mes = 'fevereiro';
+if (mes === "fevereiro") {
+    console.log("Mes dele do Carnaval")
+
+} else if (mes === "Junho") {
+    console.log("Tem ele o São João")
+
+} else if (mes === "Dezembro") {
+    console.log("Mes do Natal")
+
+} else {
+    console.log( `${mes} Não é mês festivo` )
+}*/
+
+// 
+
+console.log("Guten abend")
+const input = require('readline-sync')
+ const mes = input.question('Que mes sera festivo?')
+
+ if (mes = ) {
+     console.log("Mes do carnaval")
+ }
+ else if (mes = Junho) {
+     console.log('Mes do São João')
+
+ }
+
+ else if (mes = abril) {console.log('Mes da pascoa')
+}
+
+else if (mes = Dezembro) {console.log('Mes do natal')}
+
+else {console.log('não é mes festivo')
+}
